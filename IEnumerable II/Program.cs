95d30f4a286f1dee86c610b68688f72a3b6c4c1f
millojsg <millojsgprixus@gmail.com>
2020-06-15 09:32:20 -0400

    Se agrega proyecto Metodo de extension

---
+++ IEnumerable II/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerable_II
+{
+    //IEnumerable genericos
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int i in Potencia(2, 8))
+            {
+                Console.Write("{0} ", i);
+            }
+        }
+
+        public static System.Collections.Generic.IEnumerable<int> Potencia(int numero, int exponente)
+        {
+            int resultado = 1;
+
+            for (int i = 0; i < exponente; i++)
+            {
+                resultado = resultado * numero;
+                 yield return resultado; //Con la palabra yield regreso una coleccion de array tipo int.
+            }
+        }
+    }
+}
---
+++ Metodo de exetension/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Primer parámetro incluye el modificadoo <this>.
+//Solo en clases estaticas no anidadas no genericas.
+
+namespace Metodo_de_extension
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = { "1", "22", "333", "4444" };
+        
+            foreach (string s in Extensions.Slice(strings, 1,3))  
+            {
+                Console.WriteLine(Extensions.ToInt32(s));
+            }
+
+            //Otra manera de invocarlo
+            foreach (string s in strings.Slice(1, 2))
+            {
+                Console.WriteLine(s.ToInt32());
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public static class Extensions
+    { 
+        //Convierte de string a entero
+        public static int ToInt32(this string s)
+        {
+            return Int32.Parse(s);
+        }
+
+        //Retorna una una porcion del array que se le pase
+        public static T[] Slice<T>(this T[] source, int index, int count)
+        {
+            if (index < 0 || count < 0 || source.Length - index < count)
+                {throw new ArgumentException();}
+
+            T[] result = new T[count];
+            Array.Copy(source, index, result, 0, count);
+            return result;
+        }
+    }
+}
